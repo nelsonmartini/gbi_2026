f31374c727056544f7f4b903d8f97c2b07526c30
Nelson Martini <neilmartin.nku@gmail.com>
2026-06-25 10:43:56 -0400

    Bump cache version for Tanner avatar update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gbi2026-v6';
+const CACHE = 'gbi2026-v7';
 const ASSETS = [
   '/', '/index.html', '/gbi-logo.jpg', '/manifest.json',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
