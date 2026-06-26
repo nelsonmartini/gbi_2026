327d9ba61a1e98b27303249ff77e776067db5845
Nelson Martini <neilmartin.nku@gmail.com>
2026-06-26 00:09:20 -0400

    Revert header to original top position; tab-nav back to sticky

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gbi2026-v18';
+const CACHE = 'gbi2026-v19';
 const ASSETS = [
   '/', '/index.html', '/gbi-logo.jpg', '/manifest.json',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
