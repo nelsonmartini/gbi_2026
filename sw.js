15081bdf7d6aafb02b414164b5f350b872c6ba16
Nelson Martini <neilmartin.nku@gmail.com>
2026-06-26 00:56:09 -0400

    Add visible on-screen error catcher so real JS errors surface instead of failing silently

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gbi2026-v22';
+const CACHE = 'gbi2026-v23';
 const ASSETS = [
   '/', '/index.html', '/gbi-logo.jpg', '/manifest.json',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
