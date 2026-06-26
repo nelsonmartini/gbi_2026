c4efe9e9f70f12753ef3014d85f36e1c850380ea
Nelson Martini <neilmartin.nku@gmail.com>
2026-06-26 11:08:17 -0400

    Fix cut-off columns on Payout Leaderboard and Player Winnings tables; add sticky table headers site-wide; add Holes Won column to Day 1/2 standings

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gbi2026-v32';
+const CACHE = 'gbi2026-v33';
 const ASSETS = [
   '/', '/index.html', '/gbi-logo.jpg', '/manifest.json',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
