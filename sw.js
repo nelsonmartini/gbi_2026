1e933c9ca3793383ed6690c5cd70b019d24f88a8
Nelson Martini <neilmartin.nku@gmail.com>
2026-06-26 00:33:41 -0400

    Hard-block duplicate player selection at the data layer for 4-Man groups and 2v2 matches, not just in the dropdown UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gbi2026-v20';
+const CACHE = 'gbi2026-v21';
 const ASSETS = [
   '/', '/index.html', '/gbi-logo.jpg', '/manifest.json',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
