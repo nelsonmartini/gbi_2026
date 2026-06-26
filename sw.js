4c8d1ae0b240c0699db4e2f352abb3d35ec8ae83
Nelson Martini <neilmartin.nku@gmail.com>
2026-06-26 01:04:13 -0400

    CRITICAL FIX: sanitize/defend every team-lookup against invalid team values, root-caused via the visible error catcher (reproduced with Jordan, Moose, Nate, hole payouts, any name)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gbi2026-v23';
+const CACHE = 'gbi2026-v24';
 const ASSETS = [
   '/', '/index.html', '/gbi-logo.jpg', '/manifest.json',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
