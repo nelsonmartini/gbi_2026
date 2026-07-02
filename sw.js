fe926bd1dad9c8d25fe9df42316f166b0c112a64
Nelson Martini <neilmartin.nku@gmail.com>
2026-07-01 20:47:40 -0400

    2v2 match result banner: team-colored pill badge instead of pts text (pts are already shown per side below, colored pill is more scannable)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gbi2026-v40';
+const CACHE = 'gbi2026-v41';
 const ASSETS = [
   '/', '/index.html', '/gbi-logo.jpg', '/manifest.json',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
