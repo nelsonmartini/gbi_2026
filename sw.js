33b67a7d422db2f907b58a3f704373bd0217bb84
Nelson Martini <neilmartin.nku@gmail.com>
2026-07-01 20:34:23 -0400

    Change site PIN to 'gbi'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gbi2026-v38';
+const CACHE = 'gbi2026-v39';
 const ASSETS = [
   '/', '/index.html', '/gbi-logo.jpg', '/manifest.json',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
