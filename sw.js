829697c1b591d2edfd343c5d5ea342285b66a60e
Nelson Martini <neilmartin.nku@gmail.com>
2026-07-06 20:46:40 -0400

    Add avatars for Ty, Jordan, Zach M, and Kyle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gbi2026-v45';
+const CACHE = 'gbi2026-v46';
 const ASSETS = [
   '/', '/index.html', '/gbi-logo.jpg', '/manifest.json',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
