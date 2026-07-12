a32a97c2484f20629d0717cab4defc5bbc8f90e4
Nelson Martini <neilmartin.nku@gmail.com>
2026-07-12 10:17:03 -0400

    4-Man captain label: 'Player 1 - Captain' with Captain in italics

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gbi2026-v62';
+const CACHE = 'gbi2026-v63';
 const ASSETS = [
   '/', '/index.html', '/gbi-logo.jpg', '/manifest.json',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
