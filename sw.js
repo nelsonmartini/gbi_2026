ff23b9865bdc0cf9c1131ade1615192c427aafa0
Nelson Martini <neilmartin.nku@gmail.com>
2026-08-19 07:45:22 -0400

    Lock Teams: also locks player roster assignments, unassign buttons, and team dropdown

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gbi2026-v91';
+const CACHE = 'gbi2026-v92';
 const ASSETS = [
   '/', '/index.html', '/gbi-logo.jpg', '/manifest.json',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
